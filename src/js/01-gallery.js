e5edbe07ffc053e77468946e97fc18f43122a47f
turko-yurii <turkoburko1993@gmail.com>
2023-07-25 19:40:49 +0300

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 
 // Створення і рендер розмітки на підставі масиву даних galleryItems
 const galleryListEl = document.querySelector('.gallery');
@@ -18,11 +18,7 @@ function createGalleryItem(galleryItems) {
       ({ preview, original, description }) =>
         `<li class="gallery__item">
       <a class="gallery__link" href="${original}">
-      <img 
-      class="gallery__image" 
-      src="${preview}" 
-      alt="${description}"
-      />
+      <img class="gallery__image" src="${preview}" alt="${description}">
       </a>
       </li>`
     )
